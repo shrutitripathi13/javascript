34c19698cf7560a20540b2644f65c5de0b1d2394
Shruti <itsshrutitripathi13@gmail.com>
2020-08-18 19:05:37 +0530

    string type

--- types.js
+++ types.js
@@ -1,3 +1,9 @@
 const name = 'wes';
 const middle = "topher";
-const last = `bos`
+const last = `bos`;
+const sentence = 'she\'s soo cool';
+const song = `oh
+i
+like
+pizza`;
+const hello = "hello my name is " + last + ". Nice to meet you";
