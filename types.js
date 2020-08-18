de3a1a67cd479467a4d85ef2327bb58c52fd700c
Shruti <itsshrutitripathi13@gmail.com>
2020-08-18 19:34:00 +0530

    const types

--- types.js
+++ types.js
@@ -7,3 +7,10 @@ i
 like
 pizza`;
 const hello = "hello my name is " + last + ". Nice to meet you";
+const html = `
+   <div>
+   <h2>${name}</h2>
+   <p>${hello}</p>
+   </div>
+`;
+console.log(html);
