258217d0d628b20f27e085b792ef6241467609fc
Shruti <itsshrutitripathi13@gmail.com>
2020-08-19 19:03:21 +0530

    till object type

--- types.js
+++ types.js
@@ -1,4 +1,4 @@
-/*const name = 'wes';
+const name = 'wes';
 const middle = "topher";
 const last = `bos`;
 const sentence = 'she\'s soo cool';
@@ -13,23 +13,29 @@ const html = `
    <p>${hello}</p>
    </div>
 `;
-console.log(html);*/
+console.log(html);
 
-/*const age = 100;
+const age = 100;
 const name = 'wes';
 
 const a = 10;
-const b = 20;*/
+const b = 20;
 
-/*const myname = 'shruti';
+const myname = 'shruti';
 const myfathersname = 'ravindra';
 const mymothersname = 'vandana';
 
 const sentence = `hello my name is ${myname}. my father's name is ${myfathersname}. my mother's name is ${mymothersname} `;
-console.log(`Sentence ${sentence}`);*/
+console.log(`Sentence ${sentence}`);
 
 const gems = 20;
 const kids = 3;
 const eachkidgets = Math.floor(gems/kids);
 const dadgets = gems % kids;
 console.log(`each kid gets ${eachkidgets}`);
+
+const person = {
+    first: 'shruti',
+    last: 'tripathi',
+    age: 18
+}; 
