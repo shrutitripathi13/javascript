9b9f4c409ab9203b3ff2c1f3840acbfac0dfcfd9
Shruti <itsshrutitripathi13@gmail.com>
2020-08-25 17:22:20 +0530

    scoping

---
+++ scope.js
@@ -0,0 +1,16 @@
+const age = 100;
+
+function go() {
+    const myage = 200;
+    const hair = 'blond';
+    console.log(myage);
+    console.log(age);
+    console.log(hair);
+    
+}
+go();
+
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+
