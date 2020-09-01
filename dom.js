61930f20179d417b2111c22bd32782281eee608e
Shruti <itsshrutitripathi13@gmail.com>
2020-09-01 18:02:44 +0530

    working on classes

--- dom.js
+++ dom.js
@@ -1,6 +1,19 @@
  
-    const p = document.querySelector('p');
-    const imgs = document.querySelectorAll('.item img')
-    const heading = document.querySelector('h2');
-    heading.textContent = 'shruti is great'; 
-    console.dir(heading.textContent);
+    //const p = document.querySelector('p');
+    //const imgs = document.querySelectorAll('.item img')
+    //const heading = document.querySelector('h2');
+   // heading.textContent = 'shruti is great'; 
+    // console.dir(heading.textContent);
+
+    //classes!!
+
+    const pic = document.querySelector('.nice');
+    pic.classList.add('open');
+    pic.classList.remove('open');
+    pic.classList.toggle('round');
+    console.log(pic.classList);
+
+    function toggleRound() {
+        pic.classList.toggle('round');
+    }
+    pic.addEventListener('click', toggleRound);
