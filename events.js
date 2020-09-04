2c358906579fe64512a36936bc0e33de6f8e633a
Shruti <itsshrutitripathi13@gmail.com>
2020-09-04 11:22:41 +0530

    alternative event listener

--- events.js
+++ events.js
@@ -2,3 +2,13 @@ const butts = document.querySelector('.butts');
 butts.addEventListener('click', function(){
     console.log('IT GOT CLICKED!!');
 });
+
+//alternative
+const butts = document.querySelector('.butts');
+const coolButton = document.querySelector(.'cool');
+function handleClick(){
+    console.log('IT GOT CLICKED!!');
+}
+butts.addEventListener('click', handleClick);
+coolButton.addEventListener('click', handleClick);
+butts.removeEventListener('click',handleClick);
